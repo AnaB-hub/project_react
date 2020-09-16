6b05e83783a9152f5e047807af525e9b75a97fc5
ana.garcia <ana.bya.tvd@gmail.com>
2020-09-16 18:50:14 -0300

    Changes App.tsx

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -2,13 +2,11 @@ import React from "react";
 
 import GlobalStyles from "../src/style/global";
 
-function App() {
-  return (
-    <>
-      <h1>Hello react project</h1>
-      <GlobalStyles />
-    </>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <h1>Hello react project</h1>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
