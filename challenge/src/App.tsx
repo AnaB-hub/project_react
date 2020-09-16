595a86fff8b8e9b74b240800fd3830e8f18634ba
ana.garcia <ana.bya.tvd@gmail.com>
2020-09-16 18:49:02 -0300

    adds base project, using create-react-app and typescript

---
+++ challenge/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import GlobalStyles from "../src/style/global";
+
+function App() {
+  return (
+    <>
+      <h1>Hello react project</h1>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
